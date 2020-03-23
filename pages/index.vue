c1d07f4a0b0bb406128c912ccdf8b721306796cb
Sherin Binu <sherinbinu@hotmail.com>
2020-03-23 12:46:23 +0530

    cleanup default boilerplate

--- pages/index.vue
+++ pages/index.vue
@@ -8,31 +8,9 @@
       <h2 class="subtitle">
         My stunning Nuxt.js project
       </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
     </div>
   </div>
 </template>
-
-<script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
-</script>
 
 <style>
 /* Sample `apply` at-rules with Tailwind CSS
